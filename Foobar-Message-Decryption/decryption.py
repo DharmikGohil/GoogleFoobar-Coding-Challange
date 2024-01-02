00bd9edca17aefd7984442297b070c8c24137027
Dharmik Gohil <86297731+DharmikGohil@users.noreply.github.com>
2024-01-02 23:18:37 +0530

    Update decryption.py

--- Foobar-Message-Decryption/decryption.py
+++ Foobar-Message-Decryption/decryption.py
@@ -3,7 +3,7 @@ import base64
 MESSAGE = ''' your encrypted message ''' #your encrypted message
 
 KEY = 'your username' #I think your username is your mail address first text before @
-                      # like if mail if abc@gmail.com -> so username : abc
+                      # like if mail if abc23@gmail.com -> so username : abc23
 
 result = []
 for i, c in enumerate(base64.b64decode(MESSAGE)):
